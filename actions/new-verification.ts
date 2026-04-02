e6d88fe9211b4dddce1537a13debd01577748350
Scorpio <duskscorpio@gmail.com>
2026-04-02 16:48:10 +0800

    Fix type error in new-verification by switching to findFirst

--- actions/new-verification.ts
+++ actions/new-verification.ts
@@ -1,41 +1,41 @@
-"use server"
+"use server";
 
 import { prisma } from "@/lib/prisma";
 
 export const newVerification = async (token: string) => {
-    const existingToken = await prisma.verificationToken.findUnique({
-        where: { token }
-    });
+  const existingToken = await prisma.verificationToken.findFirst({
+    where: { token },
+  });
 
-    if (!existingToken) {
-        return { error: "Token does not exist!" };
-    }
+  if (!existingToken) {
+    return { error: "Token does not exist!" };
+  }
 
-    const hasExpired = new Date(existingToken.expires) < new Date();
+  const hasExpired = new Date(existingToken.expires) < new Date();
 
-    if (hasExpired) {
-        return { error: "Token has expired!" };
-    }
+  if (hasExpired) {
+    return { error: "Token has expired!" };
+  }
 
-    const existingUser = await prisma.user.findUnique({
-        where: { email: existingToken.email }
-    });
+  const existingUser = await prisma.user.findUnique({
+    where: { email: existingToken.email },
+  });
 
-    if (!existingUser) {
-        return { error: "Email does not exist!" };
-    }
+  if (!existingUser) {
+    return { error: "Email does not exist!" };
+  }
 
-    await prisma.user.update({
-        where: { id: existingUser.id },
-        data: { 
-            emailVerified: new Date(),
-            email: existingToken.email
-        }
-    });
+  await prisma.user.update({
+    where: { id: existingUser.id },
+    data: {
+      emailVerified: new Date(),
+      email: existingToken.email,
+    },
+  });
 
-    await prisma.verificationToken.delete({
-        where: { id: existingToken.id }
-    });
+  await prisma.verificationToken.delete({
+    where: { id: existingToken.id },
+  });
 
-    return { success: "Email verified!" };
+  return { success: "Email verified!" };
 };
